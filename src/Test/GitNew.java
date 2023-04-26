f3c0381d1ab93b484cdbab8142dff32fd1b989b1
yakup öztürk <yakupozturk29@hotmail.com>
2023-04-26 17:55:04 +0300

    thrd

--- src/Test/GitNew.java
+++ src/Test/GitNew.java
@@ -6,6 +6,7 @@ public class GitNew {
     public static void main(String[] args) {
         System.out.println("fdhdhdh");
         System.out.println("fdhdhdh");
+        System.out.println("fdhdhdh");
     }
 
 
